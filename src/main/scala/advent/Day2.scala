d8661e44c5c1c869e8f241883c7ea6b9d090eb26
kittsville <kit.maywood@ovoenergy.com>
2021-07-23 15:29:51 +0100

    Complete day 2 part 2

--- src/main/scala/advent/Day2.scala
+++ src/main/scala/advent/Day2.scala
@@ -4,15 +4,19 @@ import scala.util.matching.Regex
 
 
 object Day2 extends App with CodeGolf {
-  val total =
+  val input =
     getInput("inputs/day2")
-      .map(Password.apply)
-      .count(_.isValid)
 
-  println(total)
+  val part1 = input.map(SledRentalPassword.apply)
+    .count(_.isValid)
+  val part2 = input.map(TobogganCorpPassword.apply)
+    .count(_.isValid)
+
+  println(s"Part 1 answer: $part1")
+  println(s"Part 2 answer: $part2")
 }
 
-case class Password(letter: Char, min: Int, max: Int, password: String) {
+case class SledRentalPassword(letter: Char, min: Int, max: Int, password: String) {
   def isValid: Boolean = {
     val letterCount = password.toCharArray.count(_ == letter)
 
@@ -20,13 +24,33 @@ case class Password(letter: Char, min: Int, max: Int, password: String) {
   }
 }
 
-object Password {
-  val PasswordMatcher: Regex = """^(\d+)-(\d+) (\w): (\w+)$""".r
-
-  def apply(input: String): Password = {
+object SledRentalPassword {
+  def apply(input: String): SledRentalPassword = {
     input match {
-      case PasswordMatcher(min, max, letter, password) => Password(letter.charAt(0), min.toInt, max.toInt, password)
+      case Password.Matcher(min, max, letter, password) => SledRentalPassword(letter.charAt(0), min.toInt, max.toInt, password)
       case _ => throw new IllegalArgumentException(s"""Invalid formatting of password line: "$input"""")
     }
   }
+}
+
+case class TobogganCorpPassword(letter: Char, pos1: Int, pos2: Int, password: Array[Char]) {
+  def isValid: Boolean = {
+    val existsAtPos1 = password(pos1 - 1) == letter
+    val existsAtPos2 = password(pos2 - 1) == letter
+
+    existsAtPos1 != existsAtPos2
+  }
+}
+
+object TobogganCorpPassword {
+  def apply(input: String): TobogganCorpPassword = {
+    input match {
+      case Password.Matcher(min, max, letter, password) => TobogganCorpPassword(letter.charAt(0), min.toInt, max.toInt, password.toCharArray)
+      case _ => throw new IllegalArgumentException(s"""Invalid formatting of password line: "$input"""")
+    }
+  }
+}
+
+object Password {
+  val Matcher: Regex = """^(\d+)-(\d+) (\w): (\w+)$""".r
 }
--- src/test/scala/advent/Day2Spec.scala
+++ src/test/scala/advent/Day2Spec.scala
@@ -5,24 +5,30 @@ import org.scalatest.matchers.should.Matchers
 
 class Day2Spec extends AnyFlatSpec with Matchers {
   "Password parser" should "parse a valid password input" in {
-    Password("1-3 a: abcde") should be(Password('a', 1, 3, "abcde"))
+    SledRentalPassword("1-3 a: abcde") should be(SledRentalPassword('a', 1, 3, "abcde"))
   }
 
   it should "fail to parse an invalid input" in {
     assertThrows[IllegalArgumentException]{
-      Password("1-3 ad: abcde")
+      SledRentalPassword("1-3 ad: abcde")
     }
   }
 
-  "Password validator" should "identify valid passwords" in {
-    Password('a', 0, 0, "").isValid should be(true)
-    Password('a', 1, 1, "a").isValid should be(true)
-    Password('a', 2, 3, "aaa").isValid should be(true)
+  "SledRentalPassword validator" should "identify valid passwords" in {
+    SledRentalPassword('a', 0, 0, "").isValid should be(true)
+    SledRentalPassword('a', 1, 1, "a").isValid should be(true)
+    SledRentalPassword('a', 2, 3, "aaa").isValid should be(true)
   }
 
   it should "identify invalid passwords" in {
-    Password('a', 1, 1, "").isValid should be(false)
-    Password('a', 2, 3, "a").isValid should be(false)
-    Password('a', 2, 3, "aaaa").isValid should be(false)
+    SledRentalPassword('a', 1, 1, "").isValid should be(false)
+    SledRentalPassword('a', 2, 3, "a").isValid should be(false)
+    SledRentalPassword('a', 2, 3, "aaaa").isValid should be(false)
+  }
+
+  "TobogganCorpPassword" should "identify valid passwords" in {
+    TobogganCorpPassword('a', 1, 2, "ab".toCharArray).isValid should be(true)
+    TobogganCorpPassword('a', 1, 3, "bba".toCharArray).isValid should be(true)
+    TobogganCorpPassword('a', 2, 3, "bab".toCharArray).isValid should be(true)
   }
 }
