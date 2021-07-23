86209d3b33d60d4f3757883981b54d33aa749c7d
kittsville <kit.maywood@ovoenergy.com>
2021-07-23 12:03:06 +0100

    Complete day 1 part 2

--- src/main/scala/advent/Day1.scala
+++ src/main/scala/advent/Day1.scala
@@ -2,15 +2,29 @@ package advent
 
 object Day1 extends App with CodeGolf {
 
-  def multiply2020Entries(expenseReport: Set[Int]): Int = {
-    val firstEntry =
-      expenseReport.find(entry => expenseReport.contains(2020 - entry)).get
-    val secondEntry = 2020 - firstEntry
+  private def maybePair(entries: Set[Int], total: Int): Option[(Int, Int)] = {
+    val maybeEntry = entries.find(entry => entries.contains(total - entry))
+
+    maybeEntry.map(firstEntry => (firstEntry, total - firstEntry))
+  }
+
+  // Can't handle an input of List(1010, 1010)
+  // but it's not an edge case worth coding for
+  def multiplyTwo2020Entries(expenseReport: Set[Int]): Int = {
+    val (firstEntry, secondEntry) = maybePair(expenseReport, 2020).get
 
     firstEntry * secondEntry
   }
 
-  val total = multiply2020Entries(
+  def multiplyThree2020Entries(expenseReport: Set[Int]): Int = {
+    val firstEntry = expenseReport.find(entry => maybePair(expenseReport, 2020 - entry).isDefined).get
+
+    val (secondEntry, thirdEntry) = maybePair(expenseReport, 2020 - firstEntry).get
+
+    firstEntry * secondEntry * thirdEntry
+  }
+
+  val total = multiplyThree2020Entries(
     getInput("inputs/day1")
       .map(_.toInt)
       .toSet
--- src/test/scala/advent/Day1Spec.scala
+++ src/test/scala/advent/Day1Spec.scala
@@ -4,13 +4,19 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class Day1Spec extends AnyFlatSpec with Matchers {
-  "Day1" should "multiply two numbers that sum to 2020" in {
-    Day1.multiply2020Entries(Set(2019, 1)) should be(2019)
+  "Day1 Part 1" should "multiply two numbers that sum to 2020" in {
+    Day1.multiplyTwo2020Entries(Set(2019, 1)) should be(2019)
 
-    Day1.multiply2020Entries(Set(5, 2019, 1)) should be(2019)
+    Day1.multiplyTwo2020Entries(Set(5, 2019, 1)) should be(2019)
 
-    Day1.multiply2020Entries(Set(1721, 979, 366, 299, 675, 1456)) should be(
+    Day1.multiplyTwo2020Entries(Set(1721, 979, 366, 299, 675, 1456)) should be(
       514579
     )
+  }
+
+  "Day1 Part 2" should "multiply two numbers that sum to 2020" in {
+    Day1.multiplyThree2020Entries(Set(2017, 2, 1)) should be(4034)
+
+    Day1.multiplyThree2020Entries(Set(1721, 979, 366, 299, 675, 1456)) should be(241861950)
   }
 }
